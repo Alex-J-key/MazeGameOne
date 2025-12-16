dcf9794d107dd87f3b99ebdf53afec2d5b79d6af
Alex J Keyser <alexanderjkeyser@gmail.com>
2025-12-16 20:03:39 +0000

    Version 2 added

---
+++ MazeGameOne/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MazeGameOne
+{
+    /*
+ TCreative Commons Attribution-NonCommercial 4.0 International
+ (CC BY-NC 4.0)
+
+ Copyright © 2025 Alexander Keyser
+ https://Website-ajk.uk
+
+ This project is licensed under the Creative Commons
+ Attribution-NonCommercial 4.0 International License.
+
+ Full license text:
+ https://creativecommons.org/licenses/by-nc/4.0/
+ Or open the LICENSE File within this project
+ */
+
+    /*
+     "MazeGameOne" is a maze type game where you attempt to reach the green cell.
+     This is Version 2.0
+    */
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(942, 941);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Maze Game V2.0";
+            Paint += Form1_Paint;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
